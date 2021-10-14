cee1064a3290731711ae6111dfe13ffd16a70c7e
Alexplus77 <alexplus77@mail.ru>
2021-10-14 18:46:47 +0200

    Updates

---
+++ precache-manifest.6c8fc1489df8d71746e224e38008387d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22bbca837a48f24b1d8cb67584b452a6",
+    "url": "/calendar-react/index.html"
+  },
+  {
+    "revision": "86d63de4c4eb35883f3a",
+    "url": "/calendar-react/static/css/main.68d15eb7.chunk.css"
+  },
+  {
+    "revision": "9c3da702c24ea72f063d",
+    "url": "/calendar-react/static/js/2.91c649c7.chunk.js"
+  },
+  {
+    "revision": "c9e0c53042442321331287fb1614b34d",
+    "url": "/calendar-react/static/js/2.91c649c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86d63de4c4eb35883f3a",
+    "url": "/calendar-react/static/js/main.b03797b2.chunk.js"
+  },
+  {
+    "revision": "2c281cfb215c3a66ae4d",
+    "url": "/calendar-react/static/js/runtime-main.5cde5f97.js"
+  }
+]);
